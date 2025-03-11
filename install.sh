676d7ee42c204993f19fba91c110a6d172321045
max <9prestidigitator@gmail.com>
2025-03-11 14:44:59 -0400

    vmvmmvmvmvmvm

--- install.sh
+++ install.sh
@@ -47,10 +47,8 @@ case $sl_choice in
         pacman -S --noconfirm --needed xorg-server xorg-xinit libx11 libxft libxinerama xorgproto xorg-xrandr xorg-xev brightnessctl slock feh picom starship fastfetch keyd
         # Installing fonts
         pacman -S --noconfirm --needed ttf-jetbrains-mono ttf-jetbrains-mono-nerd noto-fonts-emoji
-        # Installing audio 
+        # Installing audio stuff
         pacman -S --noconfirm --needed pavucontrol
-        # Installing other apps
-        pacman -S --needed nautilus || true
         echo -e "\nBuilding sl programs.\n${GREEN}${BOLD}Building dwm.${RESET}\n"
         cd ./suckless/dwm/ && make clean install
         echo -e "\n${GREEN}${BOLD}Building dmenu.${RESET}\n"
@@ -65,7 +63,7 @@ case $sl_choice in
         cp -f ./.bashrc /home/$SUDO_USER/.bashrc
         cp -f ./.xinitrc /home/$SUDO_USER/.xinitrc
         # These commands cause issues apparently
-        # feh --bg-fill ./rice/wallpaper.jpg 
+        # feh --bg-fill ./rice/wallpaper.jpg
         # cp -f /.configs/keyd/default.conf /etc/keyd/default.conf
         # cp -f ./configs/picom/picom.conf /etc/xdg/picom.conf
         ;;
@@ -84,26 +82,44 @@ case $alctty_choice in
     0)
         echo -e "\nSkipping.";;
     *)
-        echo -e "Invalid input.";;
+        echo -e "\nInvalid input.";;
 esac
 
 # Install text editor: Neovim
-echo -e "\n"
-read -p "$(echo -e "${GREEN}${BOLD}Install and configure NeoVIM? [(1) Yes, (0) No]: ${RESET}")" -n 1 -r nvim_choice
-echo -e "\n"
+read -p "$(echo -e "\n${GREEN}${BOLD}Install and configure NeoVIM? [(1) Yes, (0) No]: ${RESET}\n")" -n 1 -r nvim_choice
 case $nvim_choice in
     1)
         pacman -S --noconfirm neovim npm python
         mkdir -p ~/.config/nvim && cp -fr ./configs/nvim /home/$SUDO_USER/.config/;;
     0)
-        echo "Skipping.";;
+        echo -e "\nSkipping.";;
     *)
-        echo "Invalid input.";;
+        echo -e "\nInvalid input.";;
 esac
 
-# Stuff to do other than troubleshooting:
-# keyd
-# qemu/kvm option
+# Other apps
+read -p "$(echo -e "\n${GREEN}${BOLD}Do you want to install other GUI applications? [(1) Yes, (0) No]: ${RESET}\n")" -n 1 -r apps_choice
+case $apps_choice in
+    1)
+        pacman -S --needed firefox || true
+        pacman -S --needed nautilus || true;;
+    0)
+        echo -e "\nSkipping.";;
+    *)
+        echo -e "\nInvalid input.";;
+esac
+
+# Bonus: Install Virtual Machine: QEMU/KVM
+read -p "$(echo -e "\n${GREEN}${BOLD}Do you want to install VM (QEMU/KVM)? [(1) Yes, (0) No]: ${RESET}\n")" -n 1 -r VM_choice
+case $VM_choice in
+    1)
+        pacman -S --needed qemu-full qemu-img libvirt virt-install virt-manager virt-viewer edk2-ovmf dnsmasq swtpm guestfs-tools libosinfo tuned || true;;
+        systemctl enable libvirtd.service
+    0)
+        echo -e "\nSkipping.";;
+    *)
+        echo -e "\nInvalid input.";;
+esac
 
 sys_reboot
 
