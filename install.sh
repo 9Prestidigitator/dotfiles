ce55dece619f253dfaea580f4b7ee7b578dd8121
max <9prestidigitator@gmail.com>
2025-03-10 22:23:11 -0400

    So many things wrong...

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 echo "Updating system..."
-pacman -Syu
+pacman -Syu --noconfirm
 
 echo "This Script requires GIT to be installed. Checking if GIT is installed..."
 pacman -S --noconfirm --needed base-devel git
@@ -36,11 +36,11 @@ case $choice1 in
     1)
         echo "Installing dependencies..."
         # X11 packages, slock is a basic lock screen, feh is background manager, picom is effects manager
-        pacman -S --needed xorg-server xorg-xinit libx11 libxft libxinerama xorgproto brightnessctl slock feh picom
+        pacman -S --noconfirm --needed xorg-server xorg-xinit libx11 libxft libxinerama xorgproto brightnessctl slock feh picom
         # Installing fonts
-        pacman -S ttf-jetbrains-mono noto-fonts-emoji
+        pacman -S --noconfirm --needed ttf-jetbrains-mono noto-fonts-emoji
         # Installing other apps
-        pacman -S nautilus
+        pacman -S --needed nautilus || true
         echo "Building sl programs."
         # build dwm
         cd ./suckless/dwm/
@@ -58,7 +58,7 @@ case $choice1 in
         cd ../..
         # Need to check if the line exists in the .bashrc exists in the file before appending
         # echo "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx" >> ~/.bashrc
-        cp -f ./.xinitrc ~/.xinitrc
+        cp -f ./.xinitrc home/$SUDO_USER/.xinitrc
         ;;
     0)
         echo "Skipping."
@@ -73,7 +73,7 @@ read -p "Install and configure Alacritty? [(1) Yes, (0) No]: " -n 1 -r alctty_ch
 echo -e "\n"
 case $alctty_choice in
     1)
-        pacman -S alacritty
+        pacman -S --noconfirm alacritty
         mkdir -p ~/.config/nvim && cp -fr ./configs/alacritty /home/$SUDO_USER/.config/
         ;;
     0)
@@ -89,7 +89,7 @@ read -p "Install and configure NeoVIM? [(1) Yes, (0) No]: " -n 1 -r nvim_choice
 echo -e "\n"
 case $nvim_choice in
     1)
-        pacman -S neovim npm python
+        pacman -S --noconfirm neovim npm python
         mkdir -p ~/.config/nvim && cp -fr ./configs/nvim /home/$SUDO_USER/.config/
         ;;
     0)
