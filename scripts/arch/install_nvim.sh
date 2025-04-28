8c1314cc855cd961001249b4144f4e42ee4efb3b
max <9prestidigitator@gmail.com>
2025-04-28 19:33:53 -0400

    Lol I'm bad at bash

--- scripts/arch/install_nvim.sh
+++ scripts/arch/install_nvim.sh
@@ -8,6 +8,6 @@ latexinstall="${1:-$(prompt "Install necessary latex packages? (1/0)")}"
 pacman -S --noconfirm neovim npm python
 # These packages are needed for latex editing
 if [[ $latexinstall -eq 1 ]]; then
-  pacman -S zathura zathura-pdf-poppler texlive-core texlive-binextra texlive-science
+  pacman -S --noconfirm zathura zathura-pdf-poppler texlive-core texlive-binextra texlive-science
 fi
 mkdir -p ~/.config/nvim && cp -fr ./configs/nvim /home/$SUDO_USER/.config/
