827d0c8bd8a43a054e18216ed8415e6454e97985
max <9prestidigitator@gmail.com>
2025-05-02 23:44:21 -0400

    general fixes

--- scripts/config_bash.sh
+++ scripts/config_bash.sh
@@ -14,6 +14,8 @@ bashrc_header=$(head -n 3 /home/$SUDO_USER/.bashrc || true)
 if [[ "$default_bashrc" == "$bashrc_header" ]]; then
   bluetext "Default bashrc found, replacing with my own."
   customrc=$"#\n# ~/.bashrc (system)\n#\n\nif [[ -f "/home/$SUDO_USER/dotfiles/.bashrc" ]]; then\n\tsource ~/dotfiles/.bashrc\nfi\n"
+  cp /home/$SUDO_USER/.bashrc /home/$SUDO_USER/.bashrc.bak
+  printf "%b" "$customrc" >/home/$SUDO_USER/.bashrc
 else
   echo -e "\nContains modular bashrc.\n"
 fi
