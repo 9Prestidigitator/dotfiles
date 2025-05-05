91540e5ab846257535b8be720aaa3dbf2f2e7f9d
max <9prestidigitator@gmail.com>
2025-05-05 15:01:11 -0400

    More

--- scripts/arch/install_base_pkgs.sh
+++ scripts/arch/install_base_pkgs.sh
@@ -6,19 +6,19 @@ source ./scripts/arch/paccmds.sh
 pinn brightnessctl
 
 gpud=$(detect_gpu)
-if [[ $gpud -eq "Intel" ]]; then
+if [[ "$gpud" -eq "Intel" ]]; then
   # Intel graphics requirements
   bluetext "Intel graphics detected..."
   pinn mesa mesa-utils libva-intel-driver libvdpau-va-gl vulkan-intel
-elif [[ $gpud -eq "AMD" ]]; then
+elif [[ "$gpud" -eq "AMD" ]]; then
   # AMD graphics requirements
   redtext "AMD graphics detected..."
   pinn mesa xf86-video-amdgpu vulkan-radeon libva-mesa-driver libvdpau-va-gl
-elif [[ $gpud -eq "NVIDIA" ]]; then
+elif [[ "$gpud" -eq "NVIDIA" ]]; then
   greentext "Nvidia graphics detected...more work required"
   # Nvidia graphics requirements
   pinn nvidia nvidia-utils nvidia-settings
-elif [[ $gpud -eq "Virtio" ]]; then
+elif [[ "$gpud" -eq "Virtio" ]]; then
   echo "Virtio graphics detected...more work required"
   # Virtio graphics requirements
   pinn xf86-video-qxl xf86-video-vesa xf86-video-qxl xf86-video-vesa
