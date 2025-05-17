58faf0a6b0f9b72d201d0f2809000054fae33793
max <9prestidigitator@gmail.com>
2025-05-17 02:41:32 -0400

    dwm should work now

--- scripts/arch/install_X.sh
+++ scripts/arch/install_X.sh
@@ -12,7 +12,7 @@ printf "\nInstalling dependencies...\n"
 pinn xorg-server xorg-xinit xorg-xset libx11 libxft libxinerama xorgproto xorg-xrandr autorandr xorg-xev xf86-input-evdev xclip slock maim feh picom
 
 # Picom is an effects compositor for X11
-cp -f ./configs/picom/picom.conf /etc/xdg/picom.conf
+sudo cp -f ./configs/picom/picom.conf /etc/xdg/picom.conf
 
 # run build_sl script
 ./scripts/build_sl.sh
--- scripts/build_sl.sh
+++ scripts/build_sl.sh
@@ -6,11 +6,11 @@ source ./scripts/bash_functions.sh
 root_check
 
 echo -e "\nBuilding sl programs.\n${GREEN}${BOLD}Building dwm.${RESET}\n"
-cd ./suckless/dwm/ && make clean install
+cd ./suckless/dwm/ && sudo make clean install
 echo -e "\n${GREEN}${BOLD}Building dmenu.${RESET}\n"
-cd ../dmenu/ && make clean install
+cd ../dmenu/ && sudo make clean install
 echo -e "\n${GREEN}${BOLD}Building st.${RESET}\n"
-cd ../st && make clean install
+cd ../st && sudo make clean install
 echo -e "\n${GREEN}${BOLD}Building slstatus.${RESET}\n"
-cd ../slstatus/ && make clean install && cd ../..
+cd ../slstatus/ && sudo make clean install && cd ../..
 echo -e "\n${YELLOW}Reboot required to take effect.${RESET}\n"
