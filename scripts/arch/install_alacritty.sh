363a674eb424e68274d05fce3021059a8476b6e8
max <9prestidigitator@gmail.com>
2025-05-17 02:44:44 -0400

    alacritty should work now

--- scripts/arch/install_alacritty.sh
+++ scripts/arch/install_alacritty.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 source ./scripts/bash_functions.sh
 source ./scripts/arch/paccmds.sh
 root_check
 
-pacman -S --noconfirm alacritty
-mkdir -p ~/.config/nvim && cp -fr ./configs/alacritty /home/$SUDO_USER/.config/
+pinn alacritty
+mkdir -p ~/.config/nvim && cp -fr ./configs/alacritty $HOME/.config/
