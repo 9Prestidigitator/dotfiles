e30e9bc86ee13418f784994e73a515b1a1ef2c68
max <9prestidigitator@gmail.com>
2025-05-17 02:19:15 -0400

    nvim config install fix

--- scripts/arch/install_nvim.sh
+++ scripts/arch/install_nvim.sh
@@ -11,26 +11,30 @@ source $CONFIG_FILE
 pinn neovim npm python nodejs
 [[ ${latexmk} == "on" ]] && pinn zathura zathura-pdf-poppler texlive-core texlive-binextra texlive-science
 
-
 REPO_URL="https://github.com/9Prestidigitator/nvim.git"
 TARGET_DIR="$HOME/.config/nvim"
 BACKUP_DIR="$TARGET_DIR.old"
 
-if [[ -d "$TARGET_DIR" ]]; then
-  if [[ -d "$TARGET_DIR/.git" ]]; then
-    origin_url=$(git -C "$TARGET_DIR" config --get remote.origin.url || true)
-    if [[ "$origin_url" == "$REPO_URL" ]]; then
-      printf "Updating existing config at %s\n" "$TARGET_DIR"
-      git -C "$TARGET_DIR" pull --ff-only
-      return
+install_nvim_config() {
+  if [[ -d "$TARGET_DIR" ]]; then
+    if [[ -d "$TARGET_DIR/.git" ]]; then
+      local origin_url
+      origin_url=$(git -C "$TARGET_DIR" config --get remote.origin.url || true)
+      if [[ "$origin_url" == "$REPO_URL" ]]; then
+        printf "Updating existing config at %s\n" "$TARGET_DIR"
+        git -C "$TARGET_DIR" pull --ff-only
+        return
+      fi
     fi
-  fi
 
-  printf "Existing non-repo nvim config found. Backing up to %s\n" "$BACKUP_DIR"
-  if [[ -d "$BACKUP_DIR" ]]; then
-    rm -rf "$BACKUP_DIR"
+    printf "Existing non-repo nvim config found. Backing up to %s\n" "$BACKUP_DIR"
+    if [[ -d "$BACKUP_DIR" ]]; then
+      rm -rf "$BACKUP_DIR"
+    fi
+    mv "$TARGET_DIR" "$BACKUP_DIR"
   fi
-  mv "$TARGET_DIR" "$BACKUP_DIR"
-fi
-printf "Cloning nvim config from %s to %s\n" "$REPO_URL" "$TARGET_DIR"
-git clone "$REPO_URL" "$TARGET_DIR"
+  printf "Cloning nvim config from %s to %s\n" "$REPO_URL" "$TARGET_DIR"
+  git clone "$REPO_URL" "$TARGET_DIR"
+}
+
+install_nvim_config
