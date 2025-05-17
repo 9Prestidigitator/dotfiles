6bb5438a89b5b1d5c9f461e4a76111d8d2b6bf50
max <9prestidigitator@gmail.com>
2025-05-16 21:46:36 -0400

    checking

--- scripts/arch/install_nvim.sh
+++ scripts/arch/install_nvim.sh
@@ -7,7 +7,7 @@ source ./scripts/arch/paccmds.sh
 root_check
 
 pinn neovim npm python nodejs
-prompt_run "Install necessary latex packages?" pacman -S --needed --noconfirm zathura zathura-pdf-poppler texlive-core texlive-binextra texlive-science
+prompt_run "Install necessary latex packages?" pinn zathura zathura-pdf-poppler texlive-core texlive-binextra texlive-science
 
 REPO_URL="https://github.com/9Prestidigitator/nvim.git"
 
