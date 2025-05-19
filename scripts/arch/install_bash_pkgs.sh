737e4b341e88957a2c264337839f14321e8ac045
max <9prestidigitator@gmail.com>
2025-05-19 00:47:45 -0400

    Whoops..filenames

--- scripts/arch/install_bash_pkgs.sh
+++ scripts/arch/install_bash_pkgs.sh
@@ -8,12 +8,15 @@ source ./scripts/arch/paccmds.sh
 sudo -v
 ensure_in_dir
 
+# My favorite bash packages and utilities
+pinn tmux ripgrep fd starship fastfetch figlet timeshift htop
+
 # Adding config files
 cp -f ./configs/tmux/.tmux.conf $HOME/.tmux.conf
 cp -fr ./configs/fastfetch $HOME/.config/
 
 # Installation of keyd, a really nice keyboard manager
-[[ ${keyd} == "on" ]] && pinn keyd && mkdir -p /etc/keyd && sudo cp -f /.configs/keyd/default.conf /etc/keyd/default.conf
+[[ ${keyd} == "on" ]] && pinn keyd && mkdir -p /etc/keyd && sudo cp -f ./configs/keyd/default.conf /etc/keyd/default.conf
 
 # Installing and configuring security packages
 pinn ufw fail2ban openssh
