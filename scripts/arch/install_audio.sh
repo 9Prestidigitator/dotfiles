bda8a9373e876fbbdb22d5a46d6e0897691f4e18
9prestidigitator <9prestidigitator@gmail.com>
2025-06-14 21:22:13 -0400

    sway should work now...

--- scripts/arch/install_audio.sh
+++ scripts/arch/install_audio.sh
@@ -11,10 +11,12 @@ sudo -v
 # Installing audio stuff
 pinn pipewire pipewire-pulse pipewire-jack wireplumber realtime-privileges pavucontrol qjackctl yabridge yabridgectl
 
-# There is a bug in the latest release of wine as of 06/03/2025
-# Where the UI on windows VSTs do not follow the position of the window.
-# The last known version of wine to work with yabridge is 9.21:
-sudo pacman -U ./configs/wine/wine-staging-9.21-1-x86_64.pkg.tar.zst
+# There is a bug in the latest release of wine as of 06/03/2025 Where the UI on
+# windows VSTs do not follow the position of the window. The last known version
+# of wine to work with yabridge is 9.21: There's a better fork of wine that
+# will likely lead to better performance So I gotta do some research.
+# sudo pacman -U ./configs/wine/wine-staging-9.21-1-x86_64.pkg.tar.zst
+
 # Need to add script below that makes pacman ignore updating wine.
 
 # The idea is to get an automatic install that follows:
--- scripts/other/local_to_dots.sh
+++ scripts/other/local_to_dots.sh
@@ -11,28 +11,48 @@ if [[ "$auto" != "auto" ]]; then
   prompt_run "Push local hyprland config?" cp -fr $HOME/.config/hypr ./configs/
 else
   cp -fr $HOME/.config/hypr ./configs/
+  printf "Copied hyprland configuration files to dots...\n"
 fi
 
 if [[ "$auto" != "auto" ]]; then
   prompt_run "Push local waybar config?" cp -fr $HOME/.config/waybar ./configs/
 else
   cp -fr $HOME/.config/waybar ./configs/
+  printf "Copied waybar configuration files to dots...\n"
 fi
 
 if [[ "$auto" != "auto" ]]; then
   prompt_run "Push local wofi config?" cp -fr $HOME/.config/wofi ./configs/
 else
   cp -fr $HOME/.config/wofi ./configs/
+  printf "Copied wofi configuration files to dots...\n"
 fi
 
 if [[ "$auto" != "auto" ]]; then
   prompt_run "Push local alacritty config?" cp -fr $HOME/.config/alacritty ./configs/
 else
   cp -fr $HOME/.config/alacritty ./configs/
+  printf "Copied alacritty configuration files to dots...\n"
 fi
 
 if [[ "$auto" != "auto" ]]; then
   prompt_run "Push local reaper config?" cp -fr $HOME/.config/REAPER ./configs/
 else
   cp -fr $HOME/.config/REAPER ./configs/
+  printf "Copied reaper configuration files to dots...\n"
 fi
+
+if [[ "$auto" != "auto" ]]; then
+  prompt_run "Push local dunst config?" cp -fr $HOME/.config/dunst ./configs/
+else
+  cp -fr $HOME/.config/dunst ./configs/
+  printf "Copied dunst configuration files to dots...\n"
+fi
+
+if [[ "$auto" != "auto" ]]; then
+  prompt_run "Push local fastfetch config?" cp -fr $HOME/.config/fastfetch ./configs/
+else
+  cp -fr $HOME/.config/fastfetch ./configs/
+  printf "Copied fastfetch configuration files to dots...\n"
+fi
+
